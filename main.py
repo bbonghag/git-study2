98ce45109cd2106ce4875fe961947b0309d9d032
bbonghag <leehag1224@gmail.com>
2025-11-13 16:15:46 +0900

    feat2: Add add function #1

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-def add_func(a,b):
-    return a+b
+from typing import Union
+
+def add(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+    return x + y
+if __name__=="__main__":
+    print(add(1,2))
+    print(add(1.5,2.5))
+    print(add(1,2.5))
+    print(add(1.5,2))
